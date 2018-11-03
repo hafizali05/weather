913ba7826c191802e71cd9ba3a8c31916dbc8879
hafizali05 <hafizdidarali@gmail.com>
2018-11-03 16:38:29 +0530

    avoidng muation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 const API_KEY = "7e041fbf4d0d50ea950105c5f9648feb";
 const ROOT_URL = "https://api.openweathermap.org/data/2.5/weather?";
-const FETCH_WEATHER = "FETCH_WEATHER";
+export const FETCH_WEATHER = "FETCH_WEATHER";
 import Axios from "axios";
 
 export function FetchWeather(city) {
--- src/reducers/weather_reducer.js
+++ src/reducers/weather_reducer.js
@@ -1,4 +1,14 @@
-export default function(state=null, action){
-    console.log('Action received', action);
-    return state;
+import { FETCH_WEATHER } from "../actions";
+
+export default function(state = [], action) {
+  console.log("FETCH_WEATHER", FETCH_WEATHER);
+  switch (action.type) {
+    case FETCH_WEATHER:
+      console.log("its fetch weather");
+      const res = [action.payload.data, ...state];
+      console.log(res);
+      return res;
+  }
+  // return [action.payload.data, ...state];
+  return state;
 }
